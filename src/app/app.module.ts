9bc90631993a4e64afdf38d92bde93839e2a899c
r-best <bobbyisbest3@gmail.com>
2017-11-09 15:07:55 -0500

    Setting up Bootstrap and Angular Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+const routes: Routes = [
+    {path: ``, redirectTo: `/dashboard`, pathMatch: `full`},
+    {path: 'dashboard', component: DashboardComponent},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    declarations: [
+        AppComponent,
+        DashboardComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
