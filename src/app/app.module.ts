30fa5d6f0bee5686c70cd6577104ad78432c4ba5
r-best <bobbyisbest3@gmail.com>
2017-11-09 20:20:27 -0500

    Adding spellbook component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SpellbookComponent } from './spellbook/spellbook.component';
 
 const routes: Routes = [
     {path: ``, redirectTo: `/dashboard`, pathMatch: `full`},
     {path: 'dashboard', component: DashboardComponent},
+    {path: `spellbook`, component: SpellbookComponent}
 ];
 
 @NgModule({
@@ -18,7 +20,8 @@ const routes: Routes = [
     ],
     declarations: [
         AppComponent,
-        DashboardComponent
+        DashboardComponent,
+        SpellbookComponent
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/spellbook/spellbook.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'app-spellbook',
+    templateUrl: './spellbook.component.html',
+    styleUrls: ['./spellbook.component.css'],
+    encapsulation: ViewEncapsulation.None
+})
+export class SpellbookComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+        
+    }
+
+}
