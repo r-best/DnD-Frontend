fff224585e147fb22603ccc41ba09f07a2128e7b
r-best <bobbyisbest3@gmail.com>
2017-11-20 19:26:43 -0500

    Adding layouts for add campaign route

--- src/app/campaigns/campaigns.component.ts
+++ src/app/campaigns/campaigns.component.ts
@@ -11,6 +11,10 @@ import { Subscription } from 'rxjs/Subscription';
 export class CampaignsComponent implements OnInit {
     private campaigns;
 
+    private isNewCampaignDialogueOpen: boolean = false;
+    private newCampaignNameField: string = "";
+    private badEntryAlert: boolean = false;
+
     constructor(private api: ApiService) { }
 
     ngOnInit() {
@@ -21,5 +25,36 @@ export class CampaignsComponent implements OnInit {
         this.api.GET(`/campaigns`).then((res)=>{
             this.campaigns = res;
         });
+    }
+    
+    confirmDeleteCampaign(){
+        alert(`I havent implemented this yet be patient`);
+    }
+
+    openNewCampaignDialogue(){
+        if(!this.isNewCampaignDialogueOpen)
+            this.isNewCampaignDialogueOpen = true;
+    }
+
+    closeNewCampaignDialogue(){
+        if(this.isNewCampaignDialogueOpen){
+            this.isNewCampaignDialogueOpen = false;
+            this.newCampaignNameField = "";
+            this.badEntryAlert = false;
+            this.refreshCampaignList();
+        }
+    }
+
+    createCampaign(name: string){
+        if(new RegExp("^[A-Za-z0-9-\\s]+$").test(name)){
+            if(this.badEntryAlert)
+                this.badEntryAlert = false;
+            // this.api.PUT(`/campaigns/${name}`, {}).then(res => {
+                
+            // });
+        }
+        else
+            this.badEntryAlert = true;
+            
     }
 }
